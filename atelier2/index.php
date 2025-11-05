5aaf1d701f675f274e8e13172d3d785ea2e7d8a5
lafont-loic <loic.lafont.isr@gmail.com>
2025-11-05 12:15:25 +0100

    Update index.php
    ajout

--- atelier2/index.php
+++ atelier2/index.php
@@ -18,11 +18,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Vérification simple du username et de son password.
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
     if ($username === 'admin' && $password === 'secret') {
+        $_SESSION['expectedAuthToken'] = $authToken;
         setcookie('authToken', bin2hex(random_bytes(16)), time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 minute (60 secondes) 
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
         
     } else if ($username === 'user' && $password === 'utilisateur') {
+        $_SESSION['expectedAuthToken'] = $authToken;
         setcookie('authToken', bin2hex(random_bytes(16)), time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 minute (60 secondes) 
         header('Location: page_user.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
