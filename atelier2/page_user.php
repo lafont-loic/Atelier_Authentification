a343993099b65ac8b157b3574f35605567ff3383
lafont-loic <loic.lafont.isr@gmail.com>
2025-11-05 11:50:28 +0100

    Update page_user.php

--- atelier2/page_user.php
+++ atelier2/page_user.php
@@ -4,7 +4,7 @@ session_start();
 
 // Vérifier si l'utilisateur est bien en possession d'un cookie valide
 // Dans le cas contraire il sera redirigé vers la page d'accueil de connexion
-if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== 'bin2hex(random_bytes(16))') {
+if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== bin2hex(random_bytes(16))) {
     header('Location: index.php');
     exit();
 }
